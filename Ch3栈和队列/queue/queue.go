3b0e64894446d952f753acb2adc5cac21e8bb5fe
CausingBrick <1520593807@qq.com>
2020-06-08 17:05:43 +0800

    Implementation of adding arrays and list

--- Ch3栈和队列/queue/queue.go
+++ Ch3栈和队列/queue/queue.go
@@ -1,27 +1,151 @@
-// package queue is the queue example made up by slice
+package main
 
-package queue
+import "fmt"
 
-// Queue is the common type for queue.
-type Queue []interface{}
+// QueueArr implements queue by slice.
+type QueueArr []interface{}
 
-// Enqueue push element to queue
-func (q *Queue) Enqueue(x ...interface{}) {
+// Enqueue push element to the tail of queue.
+func (q *QueueArr) Enqueue(x ...interface{}) {
 	for _, val := range x {
 		*q = append(*q, val)
 	}
 }
 
-// Dequeue pop element for queue
-func (q *Queue) Dequeue() interface{} {
-	queue := *q
-	head := queue[0]
-	*q = queue[1:]
-	return head
+// Init a queue of length 0 and capacity 10.
+func (q *QueueArr) Init() QueueArr {
+	*q = make([]interface{}, 0, 10)
+	return *q
 }
 
-// IsEmpty return if the queue is empty
-func (q *Queue) IsEmpty() bool {
+// Dequeue remove the element from the head.
+func (q *QueueArr) Dequeue() interface{} {
 	queue := *q
-	return len(queue) == 0
+	if len(queue) == 0 {
+		return nil
+	}
+	*q = queue[1:]
+	return q
+}
+
+// Search return the index of val in queue or -1 if not found.
+func (q *QueueArr) Search(val interface{}) int {
+	if len(*q) == 0 {
+		return -1
+	}
+	for k, v := range *q {
+		if v == val {
+			return k
+		}
+	}
+	return -1
+}
+
+// IsEmpty return if the queue is empty.
+func (q QueueArr) IsEmpty() bool {
+	return len(q) == 0
+}
+
+// Show print the queue.
+func (q QueueArr) Show() {
+	for _, val := range q {
+		fmt.Printf("%d <-", val)
+	}
+	fmt.Println()
+}
+
+// Element is the element of queue.
+type Element struct {
+	next, prev *Element
+	Value      interface{}
+}
+
+// Queue represents a queue implement by double linked list.
+type Queue struct {
+	// The head stores the tail with head.next and top with head.prev.
+	head Element
+	// Current queue lenght exclude (this) sintinel element.
+	len int
+}
+
+// Init initializes queue or clears s.
+func (s *Queue) Init() *Queue {
+	s.head.next = &s.head
+	s.head.prev = &s.head
+	s.len = 0
+	return s
+}
+
+// Enqueue adds a element to the tail.
+func (s *Queue) Enqueue(v ...interface{}) {
+	for i := 0; i < len(v); i++ {
+		e := &Element{s.head.next, &s.head, v[i]}
+		s.head.next.prev = e
+		s.head.next = e
+		s.len++
+	}
+}
+
+// Dequeue removes the element at head.
+func (s *Queue) Dequeue() *Element {
+	if s.len == 0 {
+		return nil
+	}
+	current := s.head.prev
+	s.head.prev = current.prev
+	s.head.prev.next = &s.head
+	current.prev, current.next = nil, nil
+	s.len--
+	return current
+}
+
+// Show prints all the element's value.
+func (s *Queue) Show() {
+	if s.len == 0 {
+		return
+	}
+	for e := s.head.next; e != &s.head; e = e.next {
+		fmt.Printf("-> %v", e.Value)
+	}
+	fmt.Println()
+}
+
+// IsEmpty return if the queue is empty.
+func (s *Queue) IsEmpty() bool {
+	return s.len == 0
+}
+
+// Search returns element stroed val or nil if not exist.
+func (s *Queue) Search(val interface{}) *Element {
+	if s.len == 0 {
+		return nil
+	}
+	for e := s.head.next; e != &s.head; e = e.next {
+		if e.Value == val {
+			return e
+		}
+	}
+	return nil
+}
+func main() {
+	fmt.Println("Test queue impelement by array")
+	q1 := new(QueueArr).Init()
+	q1.Enqueue(1, 2, 3, 4, 5, 6, 7, 8)
+	q1.Show() //1 <-2 <-3 <-4 <-5 <-6 <-7 <-8 <-
+	q1.Dequeue()
+	q1.Show()                                        //1 <-2 <-3 <-4 <-5 <-6 <-7 <-8 <-
+	fmt.Println("The index of 3 is:", q1.Search(3))  //The index of 3 is: 1
+	fmt.Println("Is the queue empty?", q1.IsEmpty()) //Is the queue empty? false
+	q1.Init()
+	fmt.Println("Is the queue empty?", q1.IsEmpty()) //Is the queue empty? true
+	fmt.Println("Test queue impelement by list")
+	q2 := new(Queue).Init()
+	q2.Enqueue(1, 2, 3, 4, 5, 6, 7, 8)
+	q2.Show() //-> 8-> 7-> 6-> 5-> 4-> 3-> 2-> 1
+	q2.Dequeue()
+	q2.Show()                                         //-> 8-> 7-> 6-> 5-> 4-> 3-> 2
+	fmt.Println("The element of 3 is:", q2.Search(3)) //The element of 3 is: &{0xc0000ae040 0xc0000ae080 3}
+	fmt.Println("Is the queue empty?", q2.IsEmpty())  //Is the queue empty? false
+	q2.Init()
+	fmt.Println("Is the queue empty?", q2.IsEmpty()) //Is the queue empty? true
 }
--- Ch3栈和队列/stack/stack.go
+++ Ch3栈和队列/stack/stack.go
@@ -1,15 +1,18 @@
-// Package stack Encapsulate a type int stack made up by slices
-package stack
+package main
 
-// Stack is the common type for stack.
-type Stack struct {
+import "fmt"
+
+// StackArr is the common type for stack.
+type StackArr struct {
 	stack []interface{}
 }
 
 // Pop return the top value of stack.
 // The length of stack will decrease.
-func (s *Stack) Pop() interface{} {
-	// fmt.Println("len:", len(s.stack))
+func (s *StackArr) Pop() interface{} {
+	if len(s.stack) < 1 {
+		return nil
+	}
 	popVal := s.stack[len(s.stack)-1]
 	temp := make([]interface{}, len(s.stack)-1)
 	copy(temp, s.stack)
@@ -19,11 +22,119 @@ func (s *Stack) Pop() interface{} {
 
 // Push will increase one unit of stack.
 // The x will be the top of stack.
-func (s *Stack) Push(x interface{}) {
-	s.stack = append(s.stack, x)
+func (s *StackArr) Push(x ...interface{}) {
+	for i := 0; i < len(x); i++ {
+		s.stack = append(s.stack, x[i])
+	}
 }
 
 // IsEmpty return the status for stack if it is empty.
-func (s *Stack) IsEmpty() bool {
+func (s *StackArr) IsEmpty() bool {
 	return len(s.stack) == 0
+}
+
+// Init a stack with cap 10.
+func (s *StackArr) Init() {
+	s.stack = make([]interface{}, 0, 10)
+}
+
+// Len return the length of stack.
+func (s *StackArr) Len() int {
+	return len(s.stack)
+}
+
+// Show return the length of stack.
+func (s *StackArr) Show() {
+	for _, val := range s.stack {
+		fmt.Printf("%v ", val)
+	}
+	fmt.Println()
+}
+
+// Element is the element of stack.
+type Element struct {
+	next, prev *Element
+	Value      interface{}
+}
+
+// Stack represents a stack implement by double linked list.
+type Stack struct {
+	// The head stores the tail with head.next and top with head.prev.
+	head Element
+	// Current stack lenght exclude (this) sintinel element.
+	len int
+}
+
+// Init initializes stack or clears s.
+func (s *Stack) Init() *Stack {
+	s.head.next = &s.head
+	s.head.prev = &s.head
+	s.len = 0
+	return s
+}
+
+// Push pushs a element to stack.
+func (s *Stack) Push(v ...interface{}) {
+	for i := 0; i < len(v); i++ {
+		e := &Element{&s.head, s.head.prev, v[i]}
+		s.head.prev.next = e
+		s.head.prev = e
+		s.len++
+	}
+}
+
+// Pop removes the top element of stack and return e.
+func (s *Stack) Pop() *Element {
+	if s.len == 0 {
+		return nil
+	}
+	current := s.head.prev
+	s.head.prev = current.prev
+	s.head.prev.next = &s.head
+	current.prev, current.next = nil, nil
+	s.len--
+	return current
+}
+
+// Show prints all the element's value.
+func (s *Stack) Show() {
+	if s.len == 0 {
+		return
+	}
+	for e := s.head.next; e != &s.head; e = e.next {
+		fmt.Printf("%v ", e.Value)
+	}
+	fmt.Println()
+}
+
+// IsEmpty return if the stack is empty.
+func (s *Stack) IsEmpty() bool {
+	return s.len == 0
+}
+
+func main() {
+	// Test stack implemented by array
+	var s1 StackArr
+	s1.Push(1, "me", 5, 6, 7)
+	s1.Show() //1 me 5 6 7
+	s1.Pop()
+	s1.Show() //1 me 5 6
+	s1.Push("u")
+	s1.Show()                                     //1 me 5 6 u
+	fmt.Println("Is stack null? :", s1.IsEmpty()) //Is stack null? : false
+	s1.Init()
+	fmt.Println("Is stack null? :", s1.IsEmpty()) //Is stack null? : true
+
+	// Test stack implemented by list
+	var s2 = new(Stack).Init()
+	s2.Push(1, "me", 5, 6, 7)
+	s2.Show() //1 me 5 6 7
+	s2.Pop()
+	s2.Show() //1 me 5 6
+	s2.Push("u")
+	s2.Show()                                     //1 me 5 6 u
+	fmt.Println("Is stack null? :", s2.IsEmpty()) //Is stack null? : false
+	s2.Init()
+	fmt.Println("Is stack null? :", s2.IsEmpty()) //Is stack null? : true
+
 }
